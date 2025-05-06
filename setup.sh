3cc356c7969967d804c064d53e24a4505a263658
Dimitri Sunaringtyas <d.sunaringtyas@outlook.com>
2025-05-06 11:14:01 +0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+mkdir -p ./tmux/plugins/
+git clone https://github.com/tmux-plugins/tpm tmux/plugins/tpm
+
 stow .
+
+mv ~/.bashrc ~/.bashrc.bak
 ln -s ~/.config/.bashrc ~/.bashrc
+
+./tmux/plugins/tpm/bin/clean_plugins
+./tmux/plugins/tpm/bin/install_plugins
+
