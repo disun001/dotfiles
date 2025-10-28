3e0693fc62ceb749b3a7740c89795601156f40db
Dimitri Sunaringtyas <d.sunaringtyas@outlook.com>
2025-10-29 01:00:19 +0700

    change: setup for omarchy

--- nvim/lua/plugins/cmp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
-      ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion with Ctrl-Space
-    })
-  end,
-}
